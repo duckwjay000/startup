e9ce2d5ca9f92bd7838a68b8b634163aed8023cb
Jaysen Duckworth <jaysen.duckworth@gmail.com>
2023-11-07 20:12:50 -0700

    Added YouTube API functionality. Gives a random video URL - and I mean RANDOM

--- public/about.js
+++ public/about.js
@@ -6,4 +6,29 @@ function getJoke() {
         });
 }
 
+const apiKey = 'AIzaSyDnrM88n-cLnUK7NeSc2zOYcGBodWFiTZ4'
+const maxResults = 1;
+
+function getVideo() {
+    fetch(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet&order=date&maxResults=${maxResults}`)
+        .then(response => response.json())
+        .then(data => {
+            // Extract the video ID from the response
+            const videoId = data.items[0].id.videoId;
+            const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+            
+            // Display the video URL or embed it in your HTML
+            console.log(videoUrl);
+
+            // You can also create an iframe to embed the video in your HTML
+            // const iframe = document.createElement('iframe');
+            // iframe.src = `https://www.youtube.com/embed/${videoId}`;
+            // document.getElementById('video-container').appendChild(iframe);
+        })
+        .catch(error => {
+            console.error('Error fetching random video:', error);
+        });
+}
+
 getJoke()
+getVideo()
