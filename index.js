f866e3fc33e7a4c6090396228943e2b0e036e868
Jaysen Duckworth <jaysen.duckworth@gmail.com>
2023-11-14 17:38:09 -0700

    Finished login functionality

--- index.js
+++ index.js
@@ -123,25 +123,3 @@ function setAuthCookie(res, authToken) {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-/*let scores = [];
-function updateScores(newScore, scores) {
-  let found = false;
-  for (const [i, prevScore] of scores.entries()) {
-    if (newScore.score > prevScore.score) {
-      scores.splice(i, 0, newScore);
-      found = true;
-      break;
-    }
-  }
-
-  if (!found) {
-    scores.push(newScore);
-  }
-
-  if (scores.length > 10) {
-    scores.length = 10;
-  }
-
-  return scores;
-}*/
