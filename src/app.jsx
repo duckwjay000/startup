047d548364edc4fcf0212e4d24acbf6640d9512d
Jaysen Duckworth <jaysen.duckworth@gmail.com>
2023-12-03 13:23:29 -0700

    Getting ready to inject

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return(
+    <div className='body bg-dark text-light'>
+        <header className="container-fluid">
+            <nav className="navbar fixed-top navbar-dark">
+                <a className="navbar-brand" href="#">Battleship Lite<sup>&trade;</sup></a>
+                <menu className="navbar-nav">
+                <li className="nav-item">
+                    <a href="index.html" className="nav-link active">Home/Login</a></li>
+                <li className="nav-item">
+                    <a href="play.html" className="nav-link">Play game</a></li>
+                <li className="nav-item">
+                    <a href="scores.html" className="nav-link">High scores</a></li>
+                <li className="nav-item">
+                    <a href="about.html" className="nav-link">About</a></li>
+                </menu>
+            </nav>
+        </header>
+
+        <main>App components go here</main>
+
+        <footer>
+            <div className="container-fluid">
+                <span className="text-reset">Link to github: </span>
+                <a className="text-reset" href="https://github.com/duckwjay000/startup.git">Ducky's github</a>
+            </div>
+        </footer>
+    </div>
+  );
 }
