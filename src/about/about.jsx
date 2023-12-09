7b54b7d05a4c5c0b972564ec9cfc5ba92b3340e5
Jaysen Duckworth <jaysen.duckworth@gmail.com>
2023-12-09 16:14:12 -0700

    Finished about section

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import './about.css'
 
-export function About() {
-  return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>about displayed here</div>
-    </main>
+export function About(props) {
+
+  // We only want this to render the first time the component is created and so we provide an empty dependency list.
+  React.useEffect(() => {
+    const apiKey = 'AIzaSyDnrM88n-cLnUK7NeSc2zOYcGBodWFiTZ4';
+    const maxResults = 1;
+    const languageCode = 'en';
+    const categoryId = '24';
+    fetch(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet&order=date&maxResults=${maxResults}&relevanceLanguage=${languageCode}`)
+      .then(response => response.json())
+      .then(data => {
+          // Extract the video ID from the response
+          const videoId = data.items[0].id.videoId;
+          const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+          
+          // Display the video URL or embed it in your HTML
+          console.log(videoUrl);
+
+          // You can also create an iframe to embed the video in your HTML
+          // const iframe = document.createElement('iframe');
+          const iframe = document.getElementById("videoFrame")
+          iframe.src = `https://www.youtube.com/embed/${videoId}`;
+          // iframe.src = `https://www.youtube.com/embed/${videoId}`;
+          // document.getElementById('video-container').appendChild(iframe);
+      })
+      .catch(error => {
+          console.error('Error fetching random video:', error);
+      });
+    }, [])
+
+
+
+
+    return (
+      <main className="container-fluid text-center">
+          <div id="picture" className="picture-box"><img src="placeholder.jpg" alt="random" /></div>
+
+          <p>
+            Battleship is a two-player strategy game where players take turns guessing the location of the others ships until all ships are sunk.
+            This version of the game is a simplified, single player version in which the player is trying to guess where the ship is in as little time and guesses as possible.
+          </p>
+    
+          <p>
+            The game Battleship is a registered trademark of HASBRO Inc. My use of the name and the game is for non-profit
+            educational use only. No part of this code or program should be used outside of that definition.
+          </p>
+          <p> Here's a random video!</p>
+          <div className="embed-responsive embed-responsive-16by9">
+            <iframe className="embed-responsive-item" id="videoFrame" src="https://www.youtube.com/embed/PtGqtow9AYs?autoplay=1"></iframe>
+          </div>
+      </main>
   );
 }
--- src/scores/scores.jsx
+++ src/scores/scores.jsx
@@ -1,9 +1,60 @@
 import React from 'react';
-
+import './scores.css'
 export function Scores() {
+  const [scores, setScores] = React.useState([]);
+
+  // Demonstrates calling a service asynchronously so that
+  // React can properly update state objects with the results.
+  React.useEffect(() => {
+    fetch('/api/scores')
+      .then((response) => response.json())
+      .then((scores) => {
+        setScores(scores);
+        localStorage.setItem('scores', JSON.stringify(scores));
+      })
+      .catch(() => {
+        const scoresText = localStorage.getItem('scores');
+        if (scoresText) {
+          setScores(JSON.parse(scoresText));
+        }
+      });
+  }, []);
+
+  // Demonstrates rendering an array with React
+  const scoreRows = [];
+  if (scores.length) {
+    for (const [i, score] of scores.entries()) {
+      scoreRows.push(
+        <tr key={i}>
+          <td>{i}</td>
+          <td>{score.name.split('@')[0]}</td>
+          <td>{score.score}</td>
+          <td>{score.date}</td>
+        </tr>
+      );
+    }
+  } else {
+    scoreRows.push(
+      <tr key='0'>
+        <td colSpan='4'>Be the first to score</td>
+      </tr>
+    );
+  }
+
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>scores displayed here</div>
+    <main className="container-fluid text-center">
+        <table className="table table-bordered table-hover">
+          <caption>High scores</caption>
+            <thead className="table-dark">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Number of guesses</th>
+                <th scope="col">Date</th>
+              </tr>
+            </thead>
+            <tbody id="scores">{scoreRows}</tbody>
+        </table>
     </main>
   );
 }
