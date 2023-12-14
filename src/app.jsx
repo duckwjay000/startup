c96d647ac592b5b1e4adff487e4116708a5d0096
Jaysen Duckworth <jaysen.duckworth@gmail.com>
2023-12-14 15:35:29 -0700

    Did as much as I could. Didn't finish everything

--- src/app.jsx
+++ src/app.jsx
@@ -2,63 +2,66 @@ import React from 'react';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
-import { Login } from './login/login';
+import createLoginComponent from './login/login'; // Import the modified function
 import { Play } from './play/play';
 import { Scores } from './scores/scores';
 import { About } from './about/about';
-import { AuthState } from './login/authState';
-
 
 export default function App() {
-  return(
+  return (
     <BrowserRouter>
-        <div className='body bg-dark text-light'>
-            <header className="container-fluid">
-                <nav className="navbar fixed-top navbar-dark">
-                    <a className="navbar-brand" href="#">Battleship Lite<sup>&trade;</sup></a>
-                    <menu className='navbar-nav'>
-                        <li className='nav-item'>
-                        <NavLink className='nav-link' to=''>
-                            Home/Login
-                        </NavLink>
-                        </li>
-                        <li className='nav-item'>
-                        <NavLink className='nav-link' to='play'>
-                            Play
-                        </NavLink>
-                        </li>
-                        <li className='nav-item'>
-                        <NavLink className='nav-link' to='scores'>
-                            Scores
-                        </NavLink>
-                        </li>
-                        <li className='nav-item'>
-                        <NavLink className='nav-link' to='about'>
-                            About
-                        </NavLink>
-                        </li>
-                    </menu>
-                </nav>
-            </header>
+      <div className='body bg-dark text-light'>
+        <header className="container-fluid">
+          <nav className="navbar fixed-top navbar-dark">
+            <a className="navbar-brand" href="#">
+              Battleship Lite<sup>&trade;</sup>
+            </a>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to=''>
+                  Home/Login
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='play'>
+                  Play
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='scores'>
+                  Scores
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='about'>
+                  About
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
 
-            <Routes>
-                <Route path='/' element={<Login />} exact />
-                <Route path='/play' element={<Play user/>} />
-                <Route path='/scores' element={<Scores />} />
-                <Route path='/about' element={<About />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
+        <Routes>
+          <Route path='/' element={createLoginComponent()} exact /> {/* Use the function here */}
+          <Route path='/play' element={<Play />} />
+          <Route path='/scores' element={<Scores />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
 
-            <footer>
-                <div className="container-fluid">
-                    <span className="text-reset">Link to github: </span>
-                    <a className="text-reset" href="https://github.com/duckwjay000/startup.git">Ducky's github</a>
-                </div>
-            </footer>
-        </div>
+        <footer>
+          <div className="container-fluid">
+            <span className="text-reset">Link to github: </span>
+            <a className="text-reset" href="https://github.com/duckwjay000/startup.git">
+              Ducky's github
+            </a>
+          </div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
+
 function NotFound() {
-    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './login.css'
 
-export function Login() {
+const Login = () => {
+  const [userName, setUserName] = useState(localStorage.getItem('userName') || '');
+  const [userInput, setUserInput] = useState({ userName: '', password: '' });
+
+  useEffect(() => {
+    setUserName(localStorage.getItem('userName') || '');
+  }, []);
+
+  const loginUser = async () => {
+    await loginOrCreate('/api/auth/login');
+  };
+
+  const createUser = async () => {
+    await loginOrCreate('/api/auth/create');
+  };
+
+  const loginOrCreate = async (endpoint) => {
+    const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ email: userInput.userName, password: userInput.password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+
+    if (response.ok) {
+      localStorage.setItem('userName', userInput.userName);
+      setUserName(userInput.userName);
+    } else {
+      const body = await response.json();
+      console.error(`⚠ Error: ${body.msg}`);
+    }
+  };
+
   return (
-    <main className='container-fluid bg-secondary text-center'>
-        <h1>Welcome to Battleship Lite!</h1>
-        
-    </main>
+    <div className="login-container">
+      <h1>Welcome to Battleship Lite!</h1>
+
+      {/* Displayed when needing authentication */}
+      <div className="login-form">
+        <div className="input-group mb-3">
+          <span className="input-group-text">@</span>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="username here"
+            value={userInput.userName}
+            onChange={(e) => setUserInput({ ...userInput, userName: e.target.value })}
+          />
+        </div>
+        <div className="input-group mb-3">
+          <span className="input-group-text">🔒</span>
+          <input
+            className="form-control"
+            type="password"
+            placeholder="password"
+            value={userInput.password}
+            onChange={(e) => setUserInput({ ...userInput, password: e.target.value })}
+          />
+        </div>
+        <button type="button" className="btn btn-primary" onClick={loginUser}>
+          Login
+        </button>
+        <button type="button" className="btn btn-primary" onClick={createUser}>
+          Create
+        </button>
+      </div>
+    </div>
   );
-}
+};
+
+export default Login;
